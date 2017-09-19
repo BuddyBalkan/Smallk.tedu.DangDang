d71285205df03ded4d15c3bf7839b89b8469a0de
SmallK <lxk1113@sina.com>
2017-09-19 11:02:05 +0800

    2017-9-19 11:01 commit

---
+++ JDBCOfLeaningPoint/JDBCtest/src/com/tedu/test/TestSelect.java
@@ -0,0 +1,19 @@
+package com.tedu.test;
+
+/**
+ * Created by SmallK on 2017/9/19.
+ * 使用JDBC从MySQL数据库中查询数据
+ */
+public class TestSelect {
+    public static void main(String[] args){
+        // 1.注册加载驱动
+
+        // 2.获得数据库的链接 Connection
+
+        // 3.获得PreparedStatement对象，预执行SQL编译
+
+        // 4.执行SQL语句，获得结果集
+
+        // 5.关闭数据库的链接
+    }
+}
