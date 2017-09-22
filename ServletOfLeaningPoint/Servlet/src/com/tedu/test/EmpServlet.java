9feff48ece96a144ccf1425e221a44b07fea16fe
SmallK <lxk1113@sina.com>
2017-09-22 09:03:09 +0800

    2017-09-22 09:02 commited

--- ServletOfLeaningPoint/Servlet/src/com/tedu/test/EmpServlet.java
+++ ServletOfLeaningPoint/Servlet/src/com/tedu/test/EmpServlet.java
@@ -6,6 +6,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by SmallK on 2017/9/21.
@@ -22,17 +29,57 @@ public class EmpServlet extends HttpServlet{
      */
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Connection connection = DBUtil.getConnection();
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
 
+        try {
+            preparedStatement = connection.prepareStatement("SELECT * FROM t_emp");
+            resultSet = preparedStatement.executeQuery();
 
-        PrintWriter out = resp.getWriter();
-        out.println("<table>\n" +
-                "\t\t<tr>\n" +
-                "\t\t\t<td>id</td>\n" +
-                "\t\t\t<td>password</td>\n" +
-                "\t\t\t<td>name</td>\n" +
-                "\t\t\t<td>birthday</td>\n" +
-                "\t\t\t<td>salary</td>\n" +
-                "\t\t</tr>\n" +
-                "\t</table>");
+            PrintWriter out = resp.getWriter();
+            out.println("<table border=\"1\" cellspacing=\"0\" align=\"center\" width=\"60%\">\n" +
+                    "\t\t<tr align=\"center\">\n" +
+                    "\t\t\t<td>id</td>\n" +
+                    "\t\t\t<td>password</td>\n" +
+                    "\t\t\t<td>name</td>\n" +
+                    "\t\t\t<td>birthday</td>\n" +
+                    "\t\t\t<td>salary</td>\n" +
+                    "\t\t</tr>\n" );
+
+            while (resultSet.next()){
+                out.println("<tr align=\"center\">\n" +
+                        "\t\t\t<td>" + resultSet.getInt("id") +
+                        "</td>\n" +
+                        "\t\t\t<td>" + resultSet.getString("password") +
+                        "</td>\n" +
+                        "\t\t\t<td>" + resultSet.getString("name") +
+                        "</td>\n" +
+                        "\t\t\t<td>" + resultSet.getDate("birthday") +
+                        "</td>\n" +
+                        "\t\t\t<td>" + resultSet.getDouble("salary") +
+                        "</td>\n" +
+                        "\t\t</tr>");
+            }
+            out.println("\t</table>");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                if (resultSet != null){
+                    resultSet.close();
+                }
+                if (preparedStatement != null){
+                    preparedStatement.close();
+                }
+                if (connection != null){
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+                System.out.println("关闭连接失败");
+            }
+        }
     }
 }
